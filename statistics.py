287cfe24365ebe200199f10b279d06a905a7a998
uwp1kor <Uppaluru.Vasundara@in.bosch.com>
2025-07-30 21:35:53 +0530

    Failing test

--- statistics.py
+++ statistics.py
@@ -7,7 +7,7 @@ def calculateStats(numbers):
   # implement the computation of statistics here and return the results
   stats = {}
   numbers_without_nan = [reading for reading in numbers if not isinstance(reading, float) or not math.isnan(reading)]
-  if numbers_without_nan and all_values_in_range(numbers_without_nan):
+  if numbers_without_nan:
     stats["avg"] = sum(numbers_without_nan) / len(numbers_without_nan)
     stats["max"] = max(numbers_without_nan)
     stats["min"] = min(numbers_without_nan)
